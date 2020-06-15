07a279b9e06501c7a05f7589d5709ef9f1d814fd
danibarria <barriareyesjosedaniel@gmail.com>
2020-06-15 11:49:28 -0300

    paginado replicas

--- services/replicas.js
+++ services/replicas.js
@@ -1,55 +1,59 @@
 const models = require('../models');
+const { paginateModel } = require('./utils')
 
-module.exports ={
-    getReplicas(args){
-        return models.Replica.findAll({
-            include:[models.Pedido, models.Hueso, models.Dinosaurio],
-            where:{
-                ...args
-            },
-            // PUEDE QUE VAYA UN INCLUDE, SINO, BORRAR ESTE COMENTARIO
+module.exports = {
+  countReplicas() {
+    return models.Replica.count()
+  },
+  getReplicas(page = 0, pageSize = 10, args) {
+    return models.Replica.findAndCountAll({
+      include: [models.Pedido, models.Hueso, models.Dinosaurio],
+      where: {
+        ...args
+      },
+      ...paginateModel({ page, pageSize })
+    });
+  },
+  getReplica(id) {
+    return models.Replica.findOne({
+      where: {
+        id
+      },
+      include: [models.Pedido, models.Hueso, models.Dinosaurio]
+      // PUEDE QUE VAYA UN INCLUDE, SINO, BORRAR ESTE COMENTARIO
+    });
+  },
+  createReplica(codigo, disponible, fecha_inicio, fecha_fin, fecha_baja, obs, PedidoId, HuesoId, DinosaurioId) {
+    return models.Replica.create({
+      codigo,
+      disponible,
+      fecha_inicio,
+      fecha_fin,
+      fecha_baja,
+      obs,
+      PedidoId,
+      HuesoId,
+      DinosaurioId
+    });
+  },
+  updateReplica(replicaReq) {
+    return models.Replica.upsert(replicaReq)
+  },
+  toggleDisponible(id) {
+    return models.Replica.findByPk(id)
+      .then((replicaEncontrada) => {
+        return replicaEncontrada.update({
+          disponible: !disponible
         });
-    },
-    getReplica(id) {
-        return models.Replica.findOne({
-            where: {
-                id
-            },
-            include:[models.Pedido, models.Hueso, models.Dinosaurio]
-            // PUEDE QUE VAYA UN INCLUDE, SINO, BORRAR ESTE COMENTARIO
+      })
+  },
+  deleteReplica(id) {
+    return models.Replica.findByPk(id)
+      .then((replicaEncontrada) => {
+        return replicaEncontrada.update({
+          fecha_baja: new Date(),
+          disponible: false
         });
-    },
-    createReplica(codigo, disponible, fecha_inicio, fecha_fin, fecha_baja, obs, PedidoId, HuesoId, DinosaurioId) {
-        return models.Replica.create({
-            codigo,
-            disponible,
-            fecha_inicio,
-            fecha_fin,
-            fecha_baja,
-            obs,
-            PedidoId,
-            HuesoId,
-            DinosaurioId
-        });
-    },
-    updateReplica(replicaReq){
-        return models.Replica.upsert(replicaReq)
-    },
-    toggleDisponible(id){
-        return models.Replica.findByPk(id)
-        .then((replicaEncontrada) => {
-            return replicaEncontrada.update({
-                disponible: !disponible
-            });
-        })
-    },
-    deleteReplica(id){
-        return models.Replica.findByPk(id)
-        .then((replicaEncontrada) => {
-            return replicaEncontrada.update({
-                fecha_baja:new Date(),
-                disponible:false
-            });
-        })
-    }
+      })
+  }
 }
