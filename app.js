89aaea00daf692edf7c3964256558a7d55f73854
danibarria <barriareyesjosedaniel@gmail.com>
2020-06-15 11:49:13 -0300

    quitadas dependencias no utilizadas

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var hbs = require('express-handlebars');
-var methodOverride = require('method-override');
-var session = require('express-session');
-var database = require('./models');
-var permisos = require('./middlewares/permisos');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var database = require('./models')
 
 //rutas
-var indexRouter = require('./routes/index');
-var dinosauriosRouter = require('./routes/dinosaurios');
-var fosilesRouter = require('./routes/fosiles');
-var subclaseRouter = require('./routes/subclases');
-var pedidosRouter = require('./routes/pedidos');
-var clientesRouter = require('./routes/clientes');
-var empleadosRouter = require('./routes/empleados');
-var guiasRouter = require('./routes/guias');
-var visitasRouter = require('./routes/visitas');
-var exhibicionesRouter = require('./routes/exhibiciones');
-var replicasRouter = require('./routes/replicas');
+var dinosauriosRouter = require('./routes/dinosaurios')
+var fosilesRouter = require('./routes/fosiles')
+var subclaseRouter = require('./routes/subclases')
+var pedidosRouter = require('./routes/pedidos')
+var clientesRouter = require('./routes/clientes')
+var empleadosRouter = require('./routes/empleados')
+var guiasRouter = require('./routes/guias')
+var visitasRouter = require('./routes/visitas')
+var exhibicionesRouter = require('./routes/exhibiciones')
+var replicasRouter = require('./routes/replicas')
 
-var app = express();
+var app = express()
 
-// view engine setup
-app.engine('hbs', hbs({defaultLayout:'main', extname:'.hbs'}));
-app.set('view engine', 'hbs');
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
+app.use(bodyParser.json())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
+var SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 app.use(session({
   store: new SequelizeStore({
@@ -50,44 +41,38 @@ app.use(session({
   cookie: {
     maxAge:1000*60*60*3, //3 horas
     sameSite: true,
-    secure: false //process.env.NODE_ENV === 'production'
+    secure: process.env.NODE_ENV === 'production'
   }
   })
-);
+)
 
-app.use('/', indexRouter); 
-app.use('/dinosaurios', dinosauriosRouter);
-app.use('/fosiles', fosilesRouter);
-app.use('/subclases', 
-  permisos.estaLogueado, 
-  permisos.permisoPara([permisos.ROLES.COLECCION]), 
-  subclaseRouter); /// solo coleccion
-app.use('/clientes', permisos.estaLogueado, clientesRouter);
-app.use('/empleados', permisos.estaLogueado, empleadosRouter);
-app.use('/guias', permisos.estaLogueado, guiasRouter);
-app.use('/pedidos', permisos.estaLogueado, pedidosRouter);
-app.use('/visitas', visitasRouter);
-app.use('/exhibiciones', exhibicionesRouter);
-app.use('/replicas', replicasRouter);
+app.use('/dinosaurios', dinosauriosRouter)
+app.use('/fosiles', fosilesRouter)
+app.use('/subclases', subclaseRouter)
+app.use('/clientes', clientesRouter)
+app.use('/empleados', empleadosRouter)
+app.use('/guias', guiasRouter)
+app.use('/pedidos', pedidosRouter)
+app.use('/visitas', visitasRouter)
+app.use('/exhibiciones', exhibicionesRouter)
+app.use('/replicas', replicasRouter)
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error',{req});
-});
+  res.status(err.status || 500)
+  res.render('error',{req})
+})
 
-app.sequelizeSessionStore = SequelizeStore;
-module.exports = app;
-
-//app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+app.sequelizeSessionStore = SequelizeStore
+module.exports = app
