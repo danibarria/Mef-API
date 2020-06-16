570ccae2e04eb6bc4bf73344c037c8e740f8bc97
danibarria <barriareyesjosedaniel@gmail.com>
2020-06-15 22:26:11 -0300

    cambios a usar y hueso

--- seeders/20190930152634-roles-usuarios.js
+++ seeders/20190930152634-roles-usuarios.js
@@ -27,8 +27,12 @@ module.exports = {
     }], {})
 
     await queryInterface.bulkInsert('Users', [{
+      email: 'admin@mef',
+      password: await bcrypt.hashSync('admin',10),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
       email: 'taller@mef',
-      // user.password = bcrypt.hashSync(user.password,10);
       password: await bcrypt.hashSync('taller',10),
       RolId: 1,
       createdAt: new Date(),
@@ -57,7 +61,70 @@ module.exports = {
       RolId: 5,
       createdAt: new Date(),
       updatedAt: new Date()
-    }])
+    }], {})
+
+    return queryInterface.bulkInsert('UsersRoles', [
+      {
+        UserId: 1,
+        RolId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 1,
+        RolId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 1,
+        RolId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 1,
+        RolId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 1,
+        RolId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 2,
+        RolId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 3,
+        RolId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 4,
+        RolId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 5,
+        RolId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        UserId: 6,
+        RolId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      ], {})    
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
